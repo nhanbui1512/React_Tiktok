30835bb16bd1a1c85b7a665e7d24f95bc3d5baa5
= <=buithiennhan0345@gmail.com>
2022-12-31 21:56:54 +0700

    update UI header

--- src/Layout/components/Header/index.js
+++ src/Layout/components/Header/index.js
@@ -1,12 +1,32 @@
-import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Header.module.scss';
 
+import images from '../../../assests/images';
+import { faCircleXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wraper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok"></img>
+                </div>
+
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
