007751e3dc80fdb7a402246069706a9f2d7fe747
= <=buithiennhan0345@gmail.com>
2023-01-18 16:28:53 +0700

    update 18.1.2023

--- src/Layout/components/Header/index.js
+++ src/Layout/components/Header/index.js
@@ -23,6 +23,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia}></FontAwesomeIcon>,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'Language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'Language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion}></FontAwesomeIcon>,
@@ -42,6 +57,16 @@ function Header() {
             setSearchResult([]);
         }, 0);
     });
+
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'Language':
+                // handle
+                break;
+            default:
+            // handle
+        }
+    };
 
     return (
         <header className={cx('wraper')}>
@@ -79,7 +104,7 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
