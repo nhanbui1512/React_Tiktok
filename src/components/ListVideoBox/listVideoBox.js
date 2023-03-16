56b4ec1ece6f5cc4cd2bc14d726fa3ea2d8a18e8
nhanbui1512 <buithiennhan0345@gmail.com>
2023-03-16 21:03:08 +0700

    load more video when user scroll to end page

--- src/components/ListVideoBox/listVideoBox.js
+++ src/components/ListVideoBox/listVideoBox.js
@@ -6,24 +6,61 @@ import Post from '../Post';
 import { useState, useEffect } from 'react';
 
 const cx = classNames.bind(styles);
-let randomNumber = Math.floor(Math.random() * 5) + 1;
+// let randomNumber = Math.floor(Math.random() * 5) + 1;
 
 function ListVideoBox() {
     const [videos, setVideos] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+    const [page, setPage] = useState(1);
+
+    console.log(videos.length);
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+        setIsFetching(true);
+    };
+
+    const fetchMoreListItems = () => {
+        console.log('fetching');
+        setTimeout(() => {
+            VideoServices.getVideos({ type: 'for-you', page: page })
+                .then((data) => {
+                    setVideos((prevState) => [...prevState, ...data]);
+                })
+                .then(() => {
+                    setPage(page + 1);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+            setIsFetching(false);
+        }, 2000);
+    };
 
     useEffect(() => {
-        VideoServices.getVideos({ type: 'for-you', page: randomNumber })
+        VideoServices.getVideos({ type: 'for-you', page: page })
             .then((data) => {
                 setVideos(data);
             })
             .catch((err) => {
                 console.log(err);
             });
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+
+    // useEffect(() => {}, []);
+
+    useEffect(() => {
+        if (!isFetching) return;
+        fetchMoreListItems();
+    }, [isFetching]);
+
     return (
         <div className={cx('content')}>
-            {videos.map((item) => {
-                return <Post isMuted={true} key={item.id} data={item}></Post>;
+            {videos.map((item, index) => {
+                return <Post isMuted={true} key={index} data={item}></Post>;
             })}
         </div>
     );
