e1e389b073409a25e27426b299eea65ea6551af1
nhanbui1512 <buithiennhan0345@gmail.com>
2023-04-24 23:59:24 +0700

    add video following page

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,4 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './following.module.scss';
+
+import ListVideoBox from '../../components/ListVideoBox/listVideoBox';
+
+const cx = classNames.bind(styles);
+
 function Following() {
-    return <h2>Following Page</h2>;
+    window.scrollTo(0, 0);
+    return (
+        <div className={cx('container')}>
+            <ListVideoBox />
+        </div>
+    );
 }
 export default Following;
--- src/service/videoServices.js
+++ src/service/videoServices.js
@@ -1,9 +1,25 @@
 import * as request from '../utils/request';
+
 export const getVideos = async ({ type, page }) => {
     try {
         const res = await request.get('videos', {
             params: {
                 type,
+                page: page,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
+
+export const getFollowingVideos = async ({ page }) => {
+    try {
+        const res = await request.get('videos', {
+            params: {
+                type: 'following',
                 page: page,
             },
         });
