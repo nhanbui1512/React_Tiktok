eefaa6a90bb34cfbc87a0fa8598b6e462eab1ea6
nhanbui1512 <buithiennhan0345@gmail.com>
2023-04-26 22:36:19 +0700

    udpate code from dev branch

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,12 +1,28 @@
 import classNames from 'classnames/bind';
 import styles from './following.module.scss';
-
+import { useNavigate } from 'react-router-dom';
 import ListVideoBox from '../../components/ListVideoBox/listVideoBox';
+import { useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Following() {
+    const navigate = useNavigate();
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    const authToken = localStorage.getItem('authToken') || getCookie('authToken');
     window.scrollTo(0, 0);
+
+    useEffect(() => {
+        if (!authToken) {
+            navigate('/login');
+        }
+    });
     return (
         <div className={cx('container')}>
             <ListVideoBox />
