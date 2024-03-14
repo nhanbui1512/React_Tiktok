63a310ad59e803c17acffdecc2285bc9b51656ed
nhanbui1512 <buithiennhan0345@gmail.com>
2024-03-14 21:58:23 +0700

    add notify in UI mobile

--- src/index.js
+++ src/index.js
@@ -11,7 +11,20 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GlobalStyles>
     <ThemeProvider>
-      {isMobile ? <div>Mobile app</div> : <App />}
+      {isMobile ? (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+          }}
+        >
+          <p> Only Support Desktop UI</p>
+        </div>
+      ) : (
+        <App />
+      )}
       {/* <App /> */}
     </ThemeProvider>
   </GlobalStyles>,
